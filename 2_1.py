4ad45406957fa5b9d69ea0b6f68439a61e1cd3ab
Julia <jkjinka@gmail.com>
2021-10-22 23:11:40 +0300

    1 task

---
+++ 2_1.py
@@ -0,0 +1,5 @@
+first_list = [5, 4.4, "привет", False, [2, 5], (4, 7), {1: "один", 2: "два"}]
+print("Мой список:", first_list)
+print(" ")  # разделение для красоты
+for a in first_list:
+    print(a, "-", type(a))
